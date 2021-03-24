8d993cf7c16dae5db68314218fef931e7b7d3266
tschuulez <72871233+tschuulez@users.noreply.github.com>
2021-03-24 11:01:03 +0100

    Aufgabe fertig: RandomPoem

--- L_01/script.js
+++ L_01/script.js
@@ -1,26 +1,30 @@
 "use strict";
 var Poem;
 (function (Poem) {
-    let names = ["Spongebob", "Sandy", "Patrick", "Mr. Krebs", "Taddeus", "Plankton"];
+    let names = ["Spongebob", "Sandy", "Patrick", "Mr.Krebs", "Taddeus", "Plankton"];
     let verbs = ["kocht", "pflanzt", "liebt", "mag", "hasst", "isst"];
     let objects = ["Bananen", "Äpfel", "Orangen", "Erdbeeren", "Pfirsiche", "Mangos"];
-    //console.log(names);
-    //console.log(verbs);
-    //console.log(objects);
     function Forschleife(_arrayName) {
         for (let i = _arrayName.length; i >= 1; i--) {
             //console.log(i);
-            getVerse(names, verbs, objects);
+            let verse = getVerse(names, verbs, objects);
             //console.log(getVerse([], [], []));
+            console.log(verse);
         }
     }
     Forschleife(names);
+    Forschleife(verbs);
+    Forschleife(objects);
     function getVerse(_subjects, _verbs, _objects) {
-        let verse = "";
-        let randomNumber = Math.floor(Math.random() * _subjects.length);
-        console.log(randomNumber);
-        //_subjects.splice(_subjects[randomNumber], 1);
-        return "Alohomora";
+        let randomNumberSubject = Math.floor(Math.random() * _subjects.length);
+        //console.log(randomNumberSubject);
+        let randomNumberVerb = Math.floor(Math.random() * _subjects.length);
+        let randomNumberObject = Math.floor(Math.random() * _subjects.length);
+        let verseTemp = _subjects[randomNumberSubject] + " " + _verbs[randomNumberVerb] + " " + _objects[randomNumberObject] + ".";
+        _subjects.splice(randomNumberSubject, 1);
+        _verbs.splice(randomNumberVerb, 1);
+        _objects.splice(randomNumberObject, 1);
+        return verseTemp;
     }
 })(Poem || (Poem = {}));
 //# sourceMappingURL=script.js.map
--- L_01/script.ts
+++ L_01/script.ts
@@ -1,31 +1,36 @@
 namespace Poem {
 
-    let names: string [] = ["Spongebob", "Sandy", "Patrick", "Mr. Krebs", "Taddeus", "Plankton"];
+    let names: string [] = ["Spongebob", "Sandy", "Patrick", "Mr.Krebs", "Taddeus", "Plankton"];
     let verbs: string[] = ["kocht", "pflanzt", "liebt", "mag", "hasst", "isst"];
     let objects: string [] = ["Bananen", "Äpfel", "Orangen", "Erdbeeren", "Pfirsiche", "Mangos"];
-
-    //console.log(names);
-    //console.log(verbs);
-    //console.log(objects);
 
     function Forschleife ( _arrayName: string []): void {
         for (let i: number = _arrayName.length; i >= 1; i --) {
             //console.log(i);
-            getVerse(names, verbs, objects);
+            let verse: string = getVerse(names, verbs, objects);
             //console.log(getVerse([], [], []));
+            console.log(verse);
         }
     } 
+
     Forschleife(names);
+    Forschleife(verbs); 
+    Forschleife(objects);
 
     function getVerse (_subjects: string [], _verbs: string [], _objects: string []): string {
 
-        let verse: string = "";
-        let randomNumber: number = Math.floor(Math.random() * _subjects.length);
-        console.log(randomNumber);
+        let randomNumberSubject: number = Math.floor(Math.random() * _subjects.length);
+        //console.log(randomNumberSubject);
+        let randomNumberVerb: number = Math.floor(Math.random() * _subjects.length);
+        let randomNumberObject: number = Math.floor(Math.random() * _subjects.length);
 
-        //_subjects.splice(_subjects[randomNumber], 1);
+        let verseTemp: string = _subjects[randomNumberSubject] + " " +   _verbs[ randomNumberVerb] + " " +  _objects[ randomNumberObject] + ".";
+
+        _subjects.splice(randomNumberSubject, 1);
+        _verbs.splice(randomNumberVerb, 1);
+        _objects.splice(randomNumberObject, 1);
   
-        return "Alohomora"; 
+        return verseTemp; 
 
     }
 
