0b7dcdba0d45b3bd61d447d0cf0593d560fe6aaf
tschuulez <72871233+tschuulez@users.noreply.github.com>
2021-04-14 14:03:44 +0200

    stil errors in console

--- L_02_EventsExercise/script/events_script.js
+++ L_02_EventsExercise/script/events_script.js
@@ -1,2 +1,36 @@
 "use strict";
+var EventsInspector;
+(function (EventsInspector) {
+    window.addEventListener("load", handleLoad);
+    function handleLoad() {
+        document.addEventListener("mousemove", setInfoBox);
+        let body = document.querySelector("body");
+        body.addEventListener("mousemove", setInfoBox);
+        body.addEventListener("click", logInfo);
+        body.addEventListener("keyup", logInfo);
+    }
+    function setInfoBox(_event) {
+        //console.log(_event);
+        let x = _event.clientX;
+        let y = _event.clientY;
+        let body = _event.target;
+        let info = document.querySelector("span");
+        info.style.top = "y" + "px";
+        info.style.left = "x" + "px";
+        info.textContent = x + "x" + y + "y";
+        body.appendChild(info);
+    }
+    function logInfo(_event) {
+        console.log(_event.type);
+        console.log(_event.target);
+        console.log(_event.currentTarget);
+        console.log(_event);
+    }
+})(EventsInspector || (EventsInspector = {}));
+/**
+document.addEventListener("load", handleLoad);
+document.addEventListener("mousemove", setInfoBox);
+document.addEventListener("click", logInfo);
+document.addEventListener("keyup", logInfo);
+**/ 
 //# sourceMappingURL=events_script.js.map
---
+++ L_02_EventsExercise/script/events_script.ts
@@ -0,0 +1,49 @@
+namespace EventsInspector {
+window.addEventListener("load", handleLoad);
+
+function handleLoad(): void {
+
+    document.addEventListener("mousemove", setInfoBox);
+
+    let body: HTMLElement = <HTMLElement>document.querySelector("body");
+    body.addEventListener("mousemove", setInfoBox);
+    body.addEventListener("click", logInfo);
+    body.addEventListener("keyup", logInfo);
+}
+
+function setInfoBox(_event: MouseEvent): void {
+    //console.log(_event);
+
+    let x: number = _event.clientX;
+    let y: number = _event.clientY;
+
+    let body: HTMLElement = <HTMLElement>_event.target;
+    let info: HTMLSpanElement = <HTMLElement>document.querySelector("span");
+
+    info.style.top = "y" + "px";
+    info.style.left = "x" + "px";
+
+    info.textContent = x + "x" + y + "y";
+
+    body.appendChild(info);
+
+
+
+}
+
+function logInfo(_event: Event): void {
+
+    console.log(_event.type);
+    console.log(_event.target);
+    console.log(_event.currentTarget);
+    console.log(_event);
+
+
+}
+}
+/**
+document.addEventListener("load", handleLoad);
+document.addEventListener("mousemove", setInfoBox);
+document.addEventListener("click", logInfo);
+document.addEventListener("keyup", logInfo);
+**/
