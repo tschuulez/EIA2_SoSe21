68f9fccb157ac654243af21e6a8a822934c98582
tschuulez <72871233+tschuulez@users.noreply.github.com>
2021-04-27 15:17:41 +0200

    oush

--- L_03_MemorySettings/script/memory.js
+++ L_03_MemorySettings/script/memory.js
@@ -1,6 +1,33 @@
 "use strict";
-var memorySettings;
-(function (memorySettings) {
-    window.addEventListener("load", handleload);
-})(memorySettings || (memorySettings = {}));
+var L_03_MemorySettings;
+(function (L_03_MemorySettings) {
+    window.addEventListener("load", handleLoad);
+    function handleLoad(_event) {
+        console.log("Start");
+        let form = document.querySelector("div#form");
+        let slider = document.querySelector("input#amount");
+        form.addEventListener("change", handleChange);
+        slider.addEventListener("input", displayAmount);
+    }
+    function handleChange(_event) {
+        // console.log(_event);
+        // let drink: HTMLSelectElement = <HTMLSelectElement>document.querySelector("select");
+        // console.log(drink.value);
+        // let inputs: NodeListOf<HTMLInputElement> = document.querySelectorAll("input");
+        // console.log(inputs);
+        let order = document.querySelector("div#order");
+        order.innerHTML = "";
+        let formData = new FormData(document.forms[0]);
+        for (let entry of formData) {
+            let item = document.querySelector("[value='" + entry[1] + "']");
+            let price = Number(item.getAttribute("price"));
+            order.innerHTML += item.name + "  € " + price;
+        }
+    }
+    function displayAmount(_event) {
+        let progress = document.querySelector("progress");
+        let amount = _event.target.value;
+        progress.value = parseFloat(amount);
+    }
+})(L_03_MemorySettings || (L_03_MemorySettings = {}));
 //# sourceMappingURL=memory.js.map
