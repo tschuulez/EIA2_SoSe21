8d5e33effe326a3ae9d7d3d3b74b81ce0fc06fd6
tschuulez <72871233+tschuulez@users.noreply.github.com>
2021-05-05 07:26:43 +0200

    zwischenpush

--- L_08_GenerativeKunst/script/script.js
+++ L_08_GenerativeKunst/script/script.js
@@ -7,28 +7,60 @@ var generativeArt;
     canvas.height = window.innerHeight;
     c.fillStyle = "#87898c";
     c.fillRect(0, 0, c.canvas.width, c.canvas.height);
-    let x = Math.random() * innerWidth;
+    let pattern = document.createElement("canvas").getContext("2d");
+    pattern.fillStyle = "#87898c";
+    pattern.fillRect(0, 0, innerWidth, innerHeight);
+    pattern.moveTo(0, 10);
+    pattern.lineTo(10, 10);
+    pattern.lineTo(20, 0);
+    pattern.lineTo(30, 0);
+    pattern.lineTo(40, 10);
+    pattern.lineTo(30, 20);
+    pattern.lineTo(20, 20);
+    pattern.lineTo(10, 10);
+    pattern.stroke();
+    c.fillStyle = c.createPattern(pattern.canvas, "repeat");
+    c.fillRect(0, 0, c.canvas.width, c.canvas.height);
+    //let x: number = Math.random() * innerWidth;
     //either positive or negative
-    let dx = 4;
+    //let dx: number = 4;
     let radius = 30;
-    let y = Math.random() * innerHeight;
-    let dy = 4;
-    function animate() {
-        requestAnimationFrame(animate);
-        c.clearRect(0, 0, innerWidth, innerHeight);
+    //let y: number = Math.random() * innerHeight;
+    //let dy: number = 4;
+    for (let i = 0; i < 200; i++) {
+        let y = Math.random() * innerHeight;
+        let x = Math.random() * innerWidth;
         c.beginPath();
         c.arc(x, y, radius, 0, Math.PI * 2, false);
-        c.strokeStyle = "#ffffff";
+        c.fillStyle = "#ffffff";
+        c.strokeStyle = "#000000";
+        c.fill();
         c.stroke();
-        if (x + radius > innerWidth || x - radius < 0) {
+    }
+    /**
+    function animate(): void {
+        requestAnimationFrame(animate);
+        
+        c.clearRect( 0, 0, innerWidth, innerHeight);
+        c.beginPath();
+        c.arc( x, y, radius, 0, Math.PI * 2, false);
+        c.strokeStyle = "#ffffff";
+        c.fillStyle = "#ffffff";
+        c.stroke();
+    
+        if ( x + radius > innerWidth || x - radius < 0 ) {
             dx = -dx;
+            
         }
-        if (y + radius > innerHeight || y - radius < 0) {
+        if ( y + radius > innerHeight || y - radius < 0) {
             dy = -dy;
         }
         x += dx;
         y += dy;
+    
     }
+    
     animate();
+    **/
 })(generativeArt || (generativeArt = {}));
 //# sourceMappingURL=script.js.map
--- L_08_GenerativeKunst/script/script.ts
+++ L_08_GenerativeKunst/script/script.ts
@@ -9,15 +9,43 @@ canvas.height =  window.innerHeight;
 c.fillStyle = "#87898c";
 c.fillRect(0, 0, c.canvas.width, c.canvas.height);
 
-let x: number = Math.random() * innerWidth;
+let pattern: CanvasRenderingContext2D = document.createElement("canvas").getContext("2d")!;
+
+
+pattern.fillStyle = "#87898c";
+pattern.fillRect(0, 0, innerWidth, innerHeight);
+pattern.moveTo(0, 10);
+pattern.lineTo(10, 10);
+pattern.lineTo(20, 0);
+pattern.lineTo(30, 0);
+pattern.lineTo(40, 10);
+pattern.lineTo(30, 20);
+pattern.lineTo(20, 20);
+pattern.lineTo(10, 10);
+pattern.stroke();
+
+c.fillStyle = c.createPattern(pattern.canvas, "repeat")!;
+c.fillRect(0, 0, c.canvas.width, c.canvas.height);
+
+//let x: number = Math.random() * innerWidth;
 //either positive or negative
-let dx: number = 4;
+//let dx: number = 4;
 let radius: number = 30;
-let y: number = Math.random() * innerHeight;
-let dy: number = 4;
+//let y: number = Math.random() * innerHeight;
+//let dy: number = 4;
 
+for ( let i: number = 0; i < 200; i++) { 
+    let y: number = Math.random() * innerHeight;
+    let x: number = Math.random() * innerWidth;
+    c.beginPath();
+    c.arc( x, y, radius, 0, Math.PI * 2, false); 
+    c.fillStyle = "#ffffff"; 
+    c.strokeStyle = "#000000";
+    c.fill();
+    c.stroke(); 
+}
 
-
+/** 
 function animate(): void { 
     requestAnimationFrame(animate); 
     
@@ -25,6 +53,7 @@ function animate(): void { 
     c.beginPath();
     c.arc( x, y, radius, 0, Math.PI * 2, false); 
     c.strokeStyle = "#ffffff";
+    c.fillStyle = "#ffffff"; 
     c.stroke(); 
 
     if ( x + radius > innerWidth || x - radius < 0 ) { 
@@ -40,5 +69,5 @@ function animate(): void { 
 }
 
 animate(); 
-
+**/
 }
