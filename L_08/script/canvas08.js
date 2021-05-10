85ecad44b757ed989563277f1ae569749c78fd47
tschuulez <72871233+tschuulez@users.noreply.github.com>
2021-05-10 15:35:16 +0200

    mountains street trees

--- L_08/script/canvas08.js
+++ L_08/script/canvas08.js
@@ -9,9 +9,67 @@ var Canvas08;
         if (!canvas)
             return;
         crc2 = canvas.getContext("2d");
+        let horizon = crc2.canvas.height * golden;
+        let streetWidthBack = 100;
+        let streetWidthFront = 600;
+        let treesOffsetBack = 15;
+        let treesOffsetFront = 100;
+        let posMountains = { x: 0, y: horizon };
+        let posStreet = { x: crc2.canvas.width / 2, y: horizon };
+        let posTreesStart = { x: posStreet.x - streetWidthBack / 2 - treesOffsetBack, y: horizon };
+        let posTreesEnd = { x: crc2.canvas.width / 2 - streetWidthFront / 2 - treesOffsetFront, y: crc2.canvas.height };
         drawBackground();
         drawSun({ x: 100, y: 75 });
         drawCloud({ x: 500, y: 125 }, { x: 250, y: 75 });
+        drawStreet(posStreet, streetWidthBack, streetWidthFront);
+        drawMountains(posMountains, 75, 200, "grey", "white");
+        drawMountains(posMountains, 50, 150, "grey", "lightgrey");
+        drawTrees(8, posTreesStart, posTreesEnd, 0.1, 0.37, 1.4);
+        posTreesStart.x = posStreet.x + streetWidthBack / 2 + treesOffsetBack;
+        posTreesEnd.x = posTreesEnd.x + streetWidthFront + 2 * treesOffsetFront;
+        drawTrees(8, posTreesStart, posTreesEnd, 0.1, 0.37, 1.4);
+    }
+    function drawTrees(_nTrees, _posStart, _posEnd, _minScale, _stepPos, _stepScale) {
+        console.log("Trees", _posStart, _posEnd);
+        let transform = crc2.getTransform();
+        let step = {
+            x: (_posEnd.x - _posStart.x) * _stepPos,
+            y: (_posEnd.y - _posStart.y) * _stepPos
+        };
+        crc2.translate(_posStart.x, _posStart.y);
+        crc2.scale(_minScale, _minScale);
+        do {
+            drawTree();
+            crc2.translate(step.x, step.y);
+            crc2.scale(_stepScale, _stepScale);
+        } while (--_nTrees > 0);
+        crc2.setTransform(transform);
+    }
+    function drawTree() {
+        console.log("Tree");
+        let nBranches = 50;
+        let maxRadius = 60;
+        let branch = new Path2D();
+        branch.arc(0, 0, maxRadius, 0, 2 * Math.PI);
+        crc2.fillStyle = "brown";
+        crc2.fillRect(0, 0, 20, -200);
+        crc2.save();
+        crc2.translate(0, -120);
+        do {
+            let y = Math.random() * 350;
+            let size = 1 - y / 700;
+            let x = (Math.random() - 0.5) * 2 * maxRadius;
+            crc2.save();
+            crc2.translate(0, -y);
+            crc2.scale(size, size);
+            crc2.translate(x, 0);
+            let colorAngle = 120 - Math.random() * 60;
+            let color = "HSLA(" + colorAngle + ", 50%, 30%, 0.5)";
+            crc2.fillStyle = color;
+            crc2.fill(branch);
+            crc2.restore();
+        } while (--nBranches > 0);
+        crc2.restore();
     }
     function drawBackground() {
         console.log("Background");
@@ -23,7 +81,7 @@ var Canvas08;
         crc2.fillRect(0, 0, crc2.canvas.width, crc2.canvas.height);
     }
     function drawSun(_position) {
-        console.log("sun", _position);
+        console.log("Sun", _position);
         let r1 = 30;
         let r2 = 150;
         let gradient = crc2.createRadialGradient(0, 0, r1, 0, 0, r2);
@@ -37,9 +95,9 @@ var Canvas08;
         crc2.restore();
     }
     function drawCloud(_position, _size) {
-        console.log("cloud", _position, _size);
-        let nParticles = 80;
-        let radiusParticle = 40;
+        console.log("Cloud", _position, _size);
+        let nParticles = 20;
+        let radiusParticle = 50;
         let particle = new Path2D();
         let gradient = crc2.createRadialGradient(0, 0, 0, 0, 0, radiusParticle);
         particle.arc(0, 0, radiusParticle, 0, 2 * Math.PI);
@@ -56,6 +114,45 @@ var Canvas08;
             crc2.fill(particle);
             crc2.restore();
         }
+        crc2.restore();
+    }
+    function drawStreet(_position, _widthBack, _widthFront) {
+        console.log("Street", _position, _widthBack, _widthFront);
+        crc2.beginPath();
+        crc2.moveTo(_position.x + _widthBack / 2, _position.y);
+        crc2.lineTo(crc2.canvas.width / 2 + _widthFront / 2, crc2.canvas.height);
+        crc2.lineTo(crc2.canvas.width / 2 - _widthFront / 2, crc2.canvas.height);
+        crc2.lineTo(_position.x - _widthBack / 2, _position.y);
+        crc2.closePath();
+        let gradient = crc2.createLinearGradient(0, _position.y, 0, crc2.canvas.height);
+        gradient.addColorStop(0, "darkgrey");
+        gradient.addColorStop(0.6, "black ");
+        crc2.fillStyle = gradient;
+        crc2.fill();
+    }
+    function drawMountains(_position, _min, _max, _colorLow, _colorHigh) {
+        console.log("Mountains", _position, _min, _max);
+        let stepMin = 50;
+        let stepMax = 150;
+        let x = 0;
+        crc2.save();
+        crc2.translate(_position.x, _position.y);
+        crc2.beginPath();
+        crc2.moveTo(0, 0);
+        crc2.lineTo(0, -_max);
+        do {
+            x += stepMin + Math.random() * (stepMax - stepMin);
+            let y = -_min - Math.random() * (_max - _min);
+            crc2.lineTo(x, y);
+        } while (x < crc2.canvas.width);
+        crc2.lineTo(x, 0);
+        crc2.closePath();
+        let gradient = crc2.createLinearGradient(0, 0, 0, -_max);
+        gradient.addColorStop(0, _colorLow);
+        gradient.addColorStop(0.7, _colorHigh);
+        crc2.fillStyle = gradient;
+        crc2.fill();
+        crc2.restore();
     }
 })(Canvas08 || (Canvas08 = {}));
 //# sourceMappingURL=canvas08.js.map
